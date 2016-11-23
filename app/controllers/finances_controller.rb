85782da133f23042c4b6b332a7daadff4ac63978
Deepak Lal Karna <deepakkarna23@gmail.com>
2016-11-23 06:26:26 +0000

    Edit and update finance

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -8,6 +8,19 @@ class FinancesController < ApplicationController
         redirect_to root_path
     end
     
+    def edit
+        @idea = Finance.find(params[:id])
+    end
+    
+    def update
+      @finance = Finance.find(params[:id])
+      if @finance.update(finance_params)
+        redirect_to root_path
+      else
+        redirect_to edit_finance_path(params[:id])
+      end
+    end
+    
     private
     
     def finance_params
